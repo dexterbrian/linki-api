68cb202128f12d8bdaef51efd71624bb1f68f8f8
= <brianwaweru627@gmail.com>
2023-09-17 22:57:26 +0300

    Adding back cookies and session middleware

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,9 @@ module LinkiApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # Adding back cookies and session middleware
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
   end
 end
