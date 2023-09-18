7efe577b6634e905c8ccce968f489119ca4cabb9
= <brianwaweru627@gmail.com>
2023-09-18 10:35:04 +0300

    Returned only username at POST /login & displayed error if user is not found

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,12 @@
 class SessionsController < ApplicationController
   def create
     user = User.find_by(username: params[:username])
-    session[:user_id] = user.id
-    render json: user
+    if user
+      session[:user_id] = user.id
+      render json: user, only: [ :username]
+    else
+      render json: { error: "User not found" }, status: :not_found
+    end
   end
 
   def destroy
