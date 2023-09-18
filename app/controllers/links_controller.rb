da8fd723f4839bdd64eeb4eaa9ff194c2b539017
= <brianwaweru627@gmail.com>
2023-09-18 10:55:52 +0300

    Enabled POST /links to create links for a user

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,5 +1,11 @@
 class LinksController < ApplicationController
   def create
+    link = Link.create(user_id: session[:user_id], title: params[:title], url: params[:url])
+    if link
+      render json: link, only: [:title, :url]
+    else
+      render json: { error: "validation errors" }, status: :not_found
+    end
   end
 
   def show
