6d0623f93ad3f2531e7b144cf68f3ce8112ea524
= <brianwaweru627@gmail.com>
2023-09-18 10:55:01 +0300

    Enabled POST /users to create users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,11 @@
 class UsersController < ApplicationController
   def create
+    user = User.create(username: params[:username], password: params[:password])
+    if user
+      render json: user, only: [:username]
+    else
+      render json: { error: "validation errors" }, status: :not_found
+    end
   end
 
   def show
