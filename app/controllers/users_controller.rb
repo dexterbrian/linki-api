018b36ac40e68bb39a1eff27eee696cd11cc2f80
= <brianwaweru627@gmail.com>
2023-09-18 10:35:20 +0300

    Returning only username

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
   def show
     user = User.find_by(id: session[:user_id])
     if user
-      render json: user
+      render json: user, only: [ :username]
     else
       render json: { error: "Not authorized" }, status: :unauthorized
     end
